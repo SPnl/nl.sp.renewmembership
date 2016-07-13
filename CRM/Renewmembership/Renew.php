0812594bf1497f1504b797116e0e9e5ccd2f27fa
Jaap Jansma <jaap@edeveloper.nl>
2016-07-13 10:51:12 +0200

    Fixed #856 verlengen van lidmaatschappen maakt nu ook bijdragen aan.

--- CRM/Renewmembership/Renew.php
+++ CRM/Renewmembership/Renew.php
@@ -62,6 +62,15 @@ LIMIT 0, 1 FOR UPDATE", array(1=>array($membership_id, 'Integer')));
       if ($contribution) {
         $result = civicrm_api3('Contribution', 'create', $contribution);
 
+        // Get Soft contributions
+        $softContributions = civicrm_api3('ContributionSoft', 'get', array('contribution_id' => $dao->contribution_id));
+        foreach($softContributions['values'] as $softContribution) {
+          $newSoftContribution = $softContribution;
+          unset($newSoftContribution['id']);
+          $newSoftContribution['contribution_id'] = $result['id'];
+          civicrm_api3('ContributionSoft', 'create', $newSoftContribution);
+        }
+
         $membershipPayment['contribution_id'] = $result['id'];
         $membershipPayment['membership_id'] = $membership_id;
         civicrm_api3('MembershipPayment', 'create', $membershipPayment);
@@ -78,12 +87,6 @@ LIMIT 0, 1 FOR UPDATE", array(1=>array($membership_id, 'Integer')));
     
     try {
       $contribution = civicrm_api3('Contribution', 'getsingle', array('id' => $contributionId));
-      $sql = "SELECT honor_contact_id, honor_type_id FROM civicrm_contribution WHERE id = %1";
-      $dao = CRM_Core_DAO::executeQuery($sql, array( 1 => array($contribution['id'], 'Integer')));
-      if ($dao->fetch() && $dao->honor_contact_id) {
-        $contribution['honor_contact_id'] = $dao->honor_contact_id;
-        $contribution['honor_type_id'] = $dao->honor_type_id;
-      }
     } catch (Exception $ex) {
       return false;
     }
